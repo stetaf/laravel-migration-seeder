33decb260a6f3b9fa2cba1c9750432224c6b1563
stetaf <s.tafuri95@gmail.com>
2021-07-19 16:30:19 +0200

    Display filtered results using same view

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,47 @@ Route::get('/', function () {
 
     return view('home', compact('packages'));
 })->name('home');
+
+Route::get('/hotel', function () {
+    $packages = Package::all();
+    $type = 'Hotel';
+
+    $filtered = $packages->filter(function ($package, $key) {
+        return strtolower($package['type']) == 'hotel';
+    });
+    
+    return view('packages.type', compact('filtered', 'type'));
+})->name('packages.hotels');
+
+Route::get('/residence', function () {
+    $packages = Package::all();
+    $type = 'Residence';
+
+    $filtered = $packages->filter(function ($package, $key) {
+        return strtolower($package['type']) == 'residence';
+    });
+    
+    return view('packages.type', compact('filtered', 'type'));
+})->name('packages.residence');
+
+Route::get('/b&b', function () {
+    $packages = Package::all();
+    $type = 'B&B';
+
+    $filtered = $packages->filter(function ($package, $key) {
+        return strtolower($package['type']) == 'b&b';
+    });
+    
+    return view('packages.type', compact('filtered', 'type'));
+})->name('packages.b&b');
+
+Route::get('/apartments', function () {
+    $packages = Package::all();
+    $type = 'Appartamenti';
+
+    $filtered = $packages->filter(function ($package, $key) {
+        return strtolower($package['type']) == 'apartments';
+    });
+    
+    return view('packages.type', compact('filtered', 'type'));
+})->name('packages.apartments');
