738891b9db8b03f9d1e7378ca5d2db6e5ba3cea7
stetaf <s.tafuri95@gmail.com>
2021-07-19 16:29:45 +0200

    Add general view for packages display

---
+++ resources/views/packages/type.blade.php
@@ -0,0 +1,30 @@
+@extends('layout.app')
+
+@section('content')
+    <div class="container">
+        <h1>I nostri {{ $type }}</h1>
+        <div class="row">
+            @foreach ($filtered as $package) 
+                <div class="col-12 col-md-6">
+                    <div class="package">
+                        <img src="{{ $package->image }}" alt="">
+                        <div class="data d-flex flex-column justify-content-between">
+                            <div class="upper">
+                                <h4>{{ $package->name }}</h4>
+                                <span class="address">{{ $package->city }}, {{ $package->address }}</span>
+                            </div>
+                            <div class="bottom">
+                                <span class="price">&euro; <strong>{{ $package->price }}</strong>/notte a persona</span>
+                                @if ($package->breakfast) 
+                                    <span class="breakfast">Colazione <strong>inclusa</strong></span>
+                                @else
+                                    <span class="breakfast">Colazione non inclusa</span>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
