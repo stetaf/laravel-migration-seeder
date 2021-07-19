d17627ea8191662c2d3518a6d59694a3123c8429
stetaf <s.tafuri95@gmail.com>
2021-07-19 16:08:30 +0200

    Add header structure

---
+++ resources/views/layout/partials/header.blade.php
@@ -0,0 +1,26 @@
+<div class="container">
+    <nav class="d-flex justify-content-between align-items-center">
+        <h1>
+            <a href="{{ route('home') }}">Boolean Holidays</a>
+        </h1>
+        <div class="links">
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Hotel</a>
+                </li>
+                <li>
+                    <a href="#">Residence</a>
+                </li>
+                <li>
+                    <a href="#">B&B</a>
+                </li>
+                <li>
+                    <a href="#">Apartments</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
