0f41538edac2f5a32f1bdf1469f001463af6d915
stetaf <s.tafuri95@gmail.com>
2021-07-19 14:57:37 +0200

    Create packages migration

---
+++ database/migrations/2021_07_19_125316_create_packages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('type');
+            $table->string('city');
+            $table->string('address');
+            $table->smallInteger('price');
+            $table->tinyInteger('breakfast');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}
